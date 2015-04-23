fcdd38493805bbd8094e8d665c4b00fcc6b0783a
Charles Painter <cpainter85@gmail.com>
2015-04-23 16:08:27 -0600

    formatting changes from adding a second publisher, now using short-description for creator because full was causing formating problem

--- app/models/creator.rb
+++ app/models/creator.rb
@@ -1,6 +1,6 @@
 class Creator < ActiveRecord::Base
 
-  has_many :issue_credits
+  has_many :issue_credits, dependent: :destroy
   has_many :issues, through: :issue_credits
 
   validates :comic_vine_creator_id, uniqueness: true
--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,7 +1,7 @@
 class Issue < ActiveRecord::Base
 
   belongs_to :volume
-  has_many :issue_credits
+  has_many :issue_credits, dependent: :destroy
   has_many :creators, through: :issue_credits
 
   validates :comic_vine_issue_id, uniqueness: true
