d29c9888388f85ff672ccc6a96ff4a0a80699729
Charles Painter <cpainter85@gmail.com>
2015-05-15 16:43:37 -0600

    correct welcome spec to correctly test whether logo images show up

--- Gemfile
+++ Gemfile
@@ -46,6 +46,7 @@ group :development, :test do
   gem 'pry'
   gem 'rspec-rails'
   gem 'capybara'
+  gem 'launchy'
   # Access an IRB console on exception pages or by using <%= console %> in views
   gem 'web-console', '~> 2.0'
 
--- spec/features/welcome_spec.rb
+++ spec/features/welcome_spec.rb
@@ -4,17 +4,32 @@ feature 'Welcome Page' do
 
   before :each do
     @publisher = create_publisher
-    @publisher2 = create_publisher(name: 'Marvel Comics', comic_vine_pub_id: 31, website: 'http//www.marvel.com', logo_url: 'http://static.site.com/marvellogo.gif', logo_thumb_url: 'http://static.site.com/marvellogo-thumb.gif')
   end
 
   scenario 'Welcome page lists all publishers' do
+    publisher2 = create_publisher(name: 'Marvel Comics', comic_vine_pub_id: 31, website: 'http//www.marvel.com', logo_url: 'http://static.site.com/marvellogo.gif', logo_thumb_url: 'http://static.site.com/marvellogo-thumb.gif')
 
     visit root_path
-
-    have_css("img[src='http://static.site.com/dkhlogo.gif']")
-    expect(page).to have_content 'Dark Horse Comics'
-    have_css("img[src='http://static.site.com/marvellogo.gif']")
-    expect(page).to have_content 'Marvel Comics'
+    expect(page).to have_css("img[src*='#{@publisher.logo_thumb_url}']")
+    expect(page).to have_content @publisher.name
+    expect(page).to have_css("img[src*='#{publisher2.logo_thumb_url}']")
+    expect(page).to have_content publisher2.name
   end
+
+  # unless I can figure out how to build pg_search database in test environment can't test search
+  # scenario 'User can search volumes from welcome page' do
+  #   volume = create_volume(@publisher)
+  #
+  #   visit root_path
+  #   fill_in 'query', with: volume.name
+  #   click_button 'Search'
+  #   # within('.search-box') { fill_in with: volume.name}
+  #
+  #   expect(current_path).to eq search_path
+  #
+  #   expect(page).to have_content volume.name
+  #   expect(page).to have_css("img[src*='#{volume.thumbnail_url}']")
+  #
+  # end
 
 end
