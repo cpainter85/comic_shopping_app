6a1fcc651cb6288128be7ed593105fb3e7c9b19b
Charles Painter <cpainter85@gmail.com>
2015-05-28 12:36:49 -0600

    added another creator show spec

--- spec/features/creator_show_spec.rb
+++ spec/features/creator_show_spec.rb
@@ -21,6 +21,20 @@ feature 'Creator show page' do
     expect(page).to have_content @creator.short_description
     expect(page).to have_css("img[src*='#{@creator.profile_picture_url}']")
     expect(page).to have_content "Credits (#{@creator.issues.count} Issue)"
+    expect(page).to have_content "#{issue.full_title_with_year} - #{@issue_credit.role}"
 
+  end
+
+  scenario 'Can visit other comics that same creator worked on' do
+    issue2 = create_issue(volume, issue_number: '2', comic_vine_issue_id: 99 )
+    issue_credit2 = create_issue_credit(issue2, @creator)
+
+    visit creator_path(@creator)
+
+    expect(page).to have_content "Credits (#{@creator.issues.count} Issues)"
+
+    click_link issue2.full_title_with_year
+
+    expect(current_path).to eq publisher_volume_issue_path(publisher,volume,issue2)
   end
 end
