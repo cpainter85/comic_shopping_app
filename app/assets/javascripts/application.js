01ae5df83ad7954dfbb6f18e6eb15e6804d1a38b
Charles Painter <cpainter85@gmail.com>
2015-05-28 20:06:23 -0600

    if long list of character appearances, expand and retract

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -72,11 +72,39 @@ $(document).ready(function () {
     $('.expand').show();
   });
 
-  // $(document).on('click', '.letter', function(){
-  //   $('.creator-section').animate({
-  //     height: 'toggle'
-  //   },800);
-  // });
+  if ($('.appearances li').length > 20) {
+    $('.appearances li').filter(function (index) {
+      return index > 15;
+    }).hide();
+    $('.appearances-expansion').append('<span class="expand-appearances glyphicon glyphicon-collapse-down"></span>')
+  };
+
+  $(document).on('click', '.expand-appearances', function () {
+    if($('.retract-appearances').length === 0) {
+      $('.appearances-expansion').append('<span class="retract-appearances glyphicon glyphicon-collapse-up"></span>');
+    }
+
+    $('.appearances li').filter(function (index) {
+      return index > 15;
+    }).slideDown('slow', function () {
+    });
+
+    $('.expand-appearances').hide();
+    $('.retract-appearances').show();
+
+  });
+
+  $(document).on('click', '.retract-appearances', function () {
+
+    $('.appearances li').filter(function (index) {
+      return index > 15;
+    }).slideUp('slow', function () {
+    });
+
+    $('.expand-appearances').show();
+    $('.retract-appearances').hide();
+
+  });
 
 });
 
