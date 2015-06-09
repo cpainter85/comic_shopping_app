c69bdd78824c8dc7bb6e852b17ada5998bc8e014
Charles Painter <cpainter85@gmail.com>
2015-06-09 12:32:38 -0600

    fix bug with description expand button not appearing

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -44,4 +44,33 @@ $(document).ready(function () {
       key.preventDefault();
     })
   }
+
+  if ($('.full-description').prop('scrollHeight') > $('.full-description').height()) {
+    $('.expansion-button').append('<span class="expand glyphicon glyphicon-collapse-down"></span>')
+  }
+  else {
+    $('.full-description').removeClass('full-description').addClass('short-description');
+  }
+
+  $(document).on('click', '.expand', function () {
+    if($('.retract').length === 0) {
+      $('.expansion-button').append('<span class="retract glyphicon glyphicon-collapse-up"></span>');
+    }
+
+    var descriptionHeight = $('.full-description').prop('scrollHeight');
+    $('.full-description').animate({
+      height: descriptionHeight
+    }, 600);
+    $('.expand').hide();
+    $('.retract').show();
+
+  });
+
+  $(document).on('click', '.retract', function () {
+    $('.full-description').animate({
+      height: 250
+    }, 600);
+    $('.retract').hide();
+    $('.expand').show();
+  });
 });
